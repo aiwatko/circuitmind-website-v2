baeca7fee6848c666a2f32dc98477dc0bd61921a
Oktawia Kata <oktawia.kata@gmail.com>
2019-09-02 22:09:37 +0200

    Transition to styled-components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,37 +1,33 @@
-import { Link } from 'gatsby';
+import { Link as GatsbyLink } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderEl = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`;
+
+const Nav = styled.nav`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
+
+const NavLink = styled(GatsbyLink)`
+  color: white;
+  text-decoration: none;
+`;
 
 interface Props {
   siteTitle: string;
 }
 
 const Header: React.SFC<Props> = ({ siteTitle }) => (
-  <header
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <HeaderEl>
+    <Nav>
+      <NavLink to="/">{siteTitle}</NavLink>
+    </Nav>
+  </HeaderEl>
 );
 
 export default Header;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,46 +1,28 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
 
+import GlobalStyle from '../materials/globalStyle';
 import Header from './header';
-import './layout.css';
 
-const Layout: React.SFC = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`;
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with{' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  );
-};
+interface Props {
+  title: string;
+}
+
+const Layout: React.SFC<Props> = ({ children, title }) => (
+  <>
+    <GlobalStyle />
+    <Header siteTitle={title} />
+    <Wrapper>
+      <main>{children}</main>
+    </Wrapper>
+  </>
+);
 
 export default Layout;

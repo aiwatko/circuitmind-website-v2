0d72fae9166f358fff084555d51205033327ed7a
Oktawia Kata <oktawia.kata@gmail.com>
2019-09-04 18:30:11 +0200

    Change component name case

---
+++ src/components/Header.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import Colors from '../materials/colors';
+import Link from './Link';
+
+interface Props {
+  isLogoHidden?: boolean;
+}
+interface NavProps {
+  isOpen: boolean;
+}
+
+const HeaderEl = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 60px;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 40px;
+`;
+
+const HamburgerButton = styled.button`
+  width: 50px;
+  height: auto;
+  padding: 0;
+  border: none;
+  background: transparent;
+
+  @media only screen and (min-width: 600px) {
+    display: none;
+  }
+`;
+
+const Nav = styled.nav<NavProps>`
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: rgb(${Colors.darkBlue});
+  z-index: 3;
+  transition: transform 0.5s ease-in-out;
+  ${({ isOpen }) => (isOpen ? 'transform: translateX(0);' : 'transform: translateX(100%);')};
+
+  @media only screen and (min-width: 600px) {
+    position: relative;
+    flex-direction: row;
+    justify-content: flex-end;
+    padding: 0;
+    background: transparent;
+    transform: translateX(0);
+  }
+`;
+
+const NavLink = styled(Link)`
+  margin: 10px 0;
+  padding: 10px 0;
+  text-decoration: none;
+  font-size: 25px;
+
+  @media only screen and (min-width: 600px) {
+    padding: 0;
+    margin: 0 0 0 20px;
+    font-size: 20px;
+  }
+`;
+
+const Header: React.SFC<Props> = ({ isLogoHidden }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <HeaderEl>
+      {
+        // Leaving this div here keeps hamburger button always in the right corner
+      }
+      <LogoWrapper>
+        {!isLogoHidden && (
+          <Link isInternal to='/'>
+            <Logo src='/img/logo.svg' alt='Logo' />
+          </Link>
+        )}
+      </LogoWrapper>
+      <HamburgerButton onClick={() => setOpen(!isOpen)}>
+        <img src='/img/hamburger.svg' alt='Hamburger menu' />
+      </HamburgerButton>
+      <Nav isOpen={isOpen}>
+        <NavLink isInternal to='/mission'>
+          mission
+        </NavLink>
+        <NavLink isInternal to='/team'>
+          team
+        </NavLink>
+        <NavLink isInternal to='/careers'>
+          careers
+        </NavLink>
+        <NavLink isInternal to='/contact'>
+          contact
+        </NavLink>
+      </Nav>
+    </HeaderEl>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import GlobalStyle from '../materials/globalStyle';
+import Header from './Header';
+import Colors from '../materials/colors';
+
+interface Props {
+  isLogoHidden?: boolean;
+}
+
+const Background = styled.div`
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  color: rgb(${Colors.white});
+  background-image: url('/img/bg.jpg');
+  background-size: cover;
+  background-position: center;
+  font-size: 20px;
+  font-weight: 300;
+  overflow-x: hidden;
+`;
+
+const Overlay = styled.div`
+  background: rgba(${Colors.darkBlue}, 0.85);
+  min-height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 760px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`;
+
+const Layout: React.SFC<Props> = ({ children, isLogoHidden }) => (
+  <>
+    <GlobalStyle />
+    <Background>
+      <Overlay>
+        <Wrapper>
+          <Header isLogoHidden={isLogoHidden} />
+          {children}
+        </Wrapper>
+      </Overlay>
+    </Background>
+  </>
+);
+
+export default Layout;
